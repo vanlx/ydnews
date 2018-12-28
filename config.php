d2b9973af5d0aa02e4edcf92e11b3f74804f1606
VanLx <44593190+vanlx@users.noreply.github.com>
2018-12-28 14:38:27 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$config = [
+"imei" => "354067080443659",
+"sign" => "b84b0173238a0e10249705f11c0aa3081d37cd92",
+"token" => "bac96e5d49716dd8d30bca107a65361c",
+"uuid" => "dd9c01e6-70e6-45c5-bf19-2f6e5202756b"
+];
